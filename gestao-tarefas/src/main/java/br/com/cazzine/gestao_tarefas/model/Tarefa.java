92fb0ee0a1317f9e0f902c6739e35baa3bc51b61
Gustavo Cazzine <cazzinengustavo@gmail.com>
2026-02-17 19:48:35 -0300

    feat: implementa service e pagina html para rodar e testar o sistema

--- gestao-tarefas/src/main/java/br/com/cazzine/gestao_tarefas/model/Tarefa.java
+++ gestao-tarefas/src/main/java/br/com/cazzine/gestao_tarefas/model/Tarefa.java
@@ -2,22 +2,22 @@ package br.com.cazzine.gestao_tarefas.model;
 
 public class Tarefa {
     // Atributos (Só os dados)
-    private int id;
+    private Integer id;
     private String descricao;
     private boolean concluida;
 
     // Construtores
     public Tarefa() {} // Obrigatório pro Spring
 
-    public Tarefa(int id, String descricao) {
+    public Tarefa(Integer id, String descricao) {
         this.id = id;
         this.descricao = descricao;
         this.concluida = false;
     }
 
     // Getters e Setters
-    public int getId() { return id; }
-    public void setId(int id) { this.id = id; }
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
     public String getDescricao() { return descricao; }
     public void setDescricao(String descricao) { this.descricao = descricao; }
     public boolean isConcluida() { return concluida; } // O Jackson ama o "is" para booleano
